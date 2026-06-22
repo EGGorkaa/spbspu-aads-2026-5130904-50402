ba116589aadb5b54ebf4b1f9ce16522ea45973c2
EGGorkaa <egor_novikov20004@mail.ru>
2026-06-22 14:22:56 +0300

    add cmds declarations

--- novikov.egor/F0/Actions.hpp
+++ novikov.egor/F0/Actions.hpp
@@ -1,7 +1,7 @@
 #ifndef ACTIONS_HPP
 #define ACTIONS_HPP
 
-#include <iostream>
+#include <iosfwd>
 #include <string>
 #include "CuckooHash.hpp"
 #include "Matrix.hpp"
@@ -11,6 +11,20 @@ namespace novikov
   using MatrixHash = CuckooHash< std::string, Matrix * >;
   void Create(std::istream &in, std::ostream &out, MatrixHash &db);
   void Show(std::istream &in, std::ostream &out, MatrixHash &db);
+  void Delete(std::istream &in, std::ostream &out, MatrixHash &db);
+  void Change(std::istream &in, std::ostream &out, MatrixHash &db);
+  void AddRow(std::istream &in, std::ostream &out, MatrixHash &db);
+  void AddCol(std::istream &in, std::ostream &out, MatrixHash &db);
+  void SwapRows(std::istream &in, std::ostream &out, MatrixHash &db);
+  void SwapCols(std::istream &in, std::ostream &out, MatrixHash &db);
+  void DeleteRow(std::istream &in, std::ostream &out, MatrixHash &db);
+  void DeleteCol(std::istream &in, std::ostream &out, MatrixHash &db);
+  void Sum(std::istream &in, std::ostream &out, MatrixHash &db);
+  void Sub(std::istream &in, std::ostream &out, MatrixHash &db);
+  void Mul(std::istream &in, std::ostream &out, MatrixHash &db);
+  void ScalarMul(std::istream &in, std::ostream &out, MatrixHash &db);
+  void Transpose(std::istream &in, std::ostream &out, MatrixHash &db);
+  void Det(std::istream &in, std::ostream &out, MatrixHash &db);
 }
 
 #endif
